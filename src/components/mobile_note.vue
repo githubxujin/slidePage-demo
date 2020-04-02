ec9b54870508b1502425ef7b745846b0b06c3860
xujin <symbio-137@crnet.com.hk>
2020-04-02 11:34:44 +0800

    移动端事件穿透

--- src/components/mobile_note.vue
+++ src/components/mobile_note.vue
@@ -1,17 +1,61 @@
 <template>
-  <div>
+  <div class="app" ref="app">
       <!-- <link rel="stylesheet" media="all and (orientation:portrait)" href="portrait.css">    竖放加载 -->
 	  <!-- <link rel="stylesheet" media="all and (orientation:landscape)" href="landscape.css">   横放加载 -->
+      <div class="content" ref="content">
+          <h1>内容</h1>
+          <p  @click="show = true" v-for="i in 12" :key="i">内容{{i}}</p>
+      </div>
+      <div ref='model' v-show="show" @click="show = false" class="model"></div>
   </div>
 </template>
 
 <script>
 export default {
     data() {
-        return {}
+        return {
+            show: false,
+            top: 0
+        }
     },
     mounted() {
+        let appdom = document.getElementsByClassName('app')[0];
+        appdom.addEventListener('scroll', function(e) {
+            e.preventDefault;
+            console.log(e.target.scrollTop);
+            this.top = e.target.scrollTop
+        })
+    },
+    watch: {
+        show(val) {
+            let model = this.$refs.model;
+            // let model = document.body;
+            console.log(model)
+             if(val){ //禁止滚动
+                // 移动端才生效
+                model.addEventListener('touchmove', function(e) {console.log(e);e.preventDefault()}, { passive: false });
+              }else { //开启滚动
+                model.removeEventListener('touchmove', function(e) {e.preventDefault()}, {passive: false});
+              }
+            let app = this.$refs.app;
+            let appdom = document.getElementsByClassName('app')[0];
 
+            console.log(app.scrollY)
+            console.log('appdom', appdom)
+            // 移动端，PC端都能生效
+            // if (val) {
+                
+
+            //     app.style.position = 'fixed';
+
+            //     app.style.top = -this.top + 'px'
+            // } else {
+            //     app.style.position = ''
+            //     app.style.top = ''
+
+            //     window.scrollTo(0, this.top) // 回到原先的top
+            // }
+        }
     },
     methods: {
 
@@ -20,6 +64,25 @@ export default {
 </script>
 
 <style scoped >
+.app{
+    height: 100vh;
+    width: 100vw;
+    overflow: auto;
+}
+.model{
+    position: fixed;
+    top:0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: rgba(0, 0, 0, .6);
+    z-index: 10;
+}
+p{
+    height: 20vw;
+    line-height: 20vw;
+    border-bottom: 1px solid #efefef;
+}
 /* 消除transition闪屏 */
 -webkit-transform-style: preserve-3d;
 /*设置内嵌的元素在 3D 空间如何呈现：保留 3D*/
